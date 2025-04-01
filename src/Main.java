2a4e619c70285ca33fbbab76729dc2ee6accf0c8
Leetisdeep <aaaooogjg@gmail.com>
2025-04-01 10:54:44 +0300

    homework 7

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        byte clientOS = 0;
-        short clientDeviceYear = 2017;
 
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }else  {
-            System.out.println("Установите версию приложения для Android по ссылке");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
         System.out.println();
 
-        if (clientDeviceYear < 2015) {
-            if (clientOS == 0) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-        }else {
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }else  {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
         }
         System.out.println();
 
-        short year = 2000;
-
-        if (year <= 1584 || (year % 400 != 0 && year % 100 == 0) || year % 4 != 0) {
-            System.out.println(year+" год не является високосным");
-        } else {
-            System.out.println(year+" год является високосным");
+        for (int i = 0; i <= 17; i+=2) {
+            System.out.println(i);
         }
         System.out.println();
 
-        short deliveryDistance = 95;
-
-        if (deliveryDistance > 100) {
-            System.out.println("доставки нет");
-        }else if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: 1");
-        }else if (deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: 2");
-        }else {
-            System.out.println("Потребуется дней: 3");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
         }
         System.out.println();
 
-        byte monthNumber = 12;
-
-        switch (monthNumber) {
-            case 12:
-            case 1:
-            case 2:
-                System.out.println("зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("осень");
-                break;
-            default:
-                System.out.println("неизвестно");
+        for (int i = 1904; i <= 2096; i+=4) {
+            System.out.println(i + " год является високосным");
         }
+        System.out.println();
 
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 512; i*=2) {
+            System.out.println(i);
+        }
+        System.out.println();
+
+        double salary = 0;
+        for (int i = 1; i <= 12; i++) {
+            salary += 29000;
+            System.out.println("Месяц "+i+", сумма накоплений равна "+salary+" рублей");
+        }
+        System.out.println();
+
+        salary = 0;
+        for (int i = 1; i <= 12; i++) {
+            salary = salary + 29000 + (salary * 0.01);
+            System.out.println("Месяц "+i+", сумма накоплений равна "+salary+" рублей");
+        }
+        System.out.println();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2*"+i+"="+i*2);
+        }
+        System.out.println();
 
     }
 }
