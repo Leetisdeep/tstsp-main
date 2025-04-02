1f2c412ce9c82159ab75c8953ed743e8f0f6b06e
Leetisdeep <aaaooogjg@gmail.com>
2025-04-02 11:06:08 +0300

    homework 9

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,53 @@
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 10){
-            i++;
-            System.out.print(i+" ");
+        int [] nums = new int [3];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+
+        double [] doubles = {1.57, 7.654, 9.986};
+
+        int [] array = {1,1,2,3,5,8};
+
+        System.out.println(Arrays.toString(nums).replace("[", "").replace("]", ""));
+        System.out.println(Arrays.toString(doubles).replace("[", "").replace("]", ""));
+        System.out.println(Arrays.toString(array).replace("[", "").replace("]", ""));
+        System.out.println();
+
+        for (int i = 0; i < nums.length/2; i++) {
+            int temp = nums[i];
+            nums[i] = nums[nums.length - i - 1];
+            nums[nums.length - i - 1] = temp;
         }
-        System.out.println();
-        for (; i >= 1;i--) {
-            System.out.print(i+" ");
+
+        for (int i = 0; i < doubles.length/2; i++) {
+            double temp = doubles[i];
+            doubles[i] = doubles[doubles.length - i - 1];
+            doubles[doubles.length - i - 1] = temp;
         }
-        System.out.println();
-        System.out.println();
 
-        long population = 12_000_000;
-        int j = 1;
-        for (; j <= 10; j++) {
-            long deathsAmount = population / 1000 * 8;
-            long bornAmount = population / 1000 * 17;
-
-            population -= deathsAmount;
-            population += bornAmount;
-
-            System.out.println("Год "+j+", численность населения составляет "+population);
+        for (int i = 0; i < array.length/2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
+
+        System.out.println(Arrays.toString(nums).replace("[", "").replace("]", ""));
+        System.out.println(Arrays.toString(doubles).replace("[", "").replace("]", ""));
+        System.out.println(Arrays.toString(array).replace("[", "").replace("]", ""));
         System.out.println();
 
-        double bank = 15_000;
-        int mountsAmount = 0;
-        while (bank < 12_000_000){
-            mountsAmount++;
-            bank += bank * 0.07;
-            System.out.println(bank);
-        }
-        System.out.println(mountsAmount);
-        System.out.println();
-
-        bank = 15_000;
-        mountsAmount = 0;
-        while (bank < 12_000_000){
-            mountsAmount++;
-            bank += bank * 0.07;
-            if (mountsAmount % 6 == 0) {
-                System.out.println(bank);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i] += 1;
             }
         }
-        System.out.println(mountsAmount);
-
-        bank = 15_000;
-        mountsAmount = 0;
-        for (int m=1; m <= 9 * 12; m++){
-            mountsAmount++;
-            bank += bank * 0.07;
-            if (mountsAmount % 6 == 0) {
-                System.out.println(bank);
-            }
-        }
-        System.out.println();
-
-        byte firstFriday = 4;
-        for (int m=0;m <= 3;m++){
-            System.out.println("Сегодня пятница, "+(firstFriday + 7*m)+"-е число. Необходимо подготовить отчет");
-        }
-        if (firstFriday <= 3){
-            System.out.println("Сегодня пятница, "+(firstFriday + 7*4)+"-е число. Необходимо подготовить отчет");
-        }
-        System.out.println();
-
-        int year = 2025;
-        int cometFly = 79;
-        int negativeInterval = 200;
-        int positiveInterval = 100;
-
-        int cometYear = year - year % cometFly;
-        int timesCometFlewBefore = (negativeInterval-(year-cometYear)) / cometFly;
-        int timesCometFlewAfter = (year+positiveInterval-cometYear) / cometFly;
-
-        for (int tc=timesCometFlewBefore;tc >= 0;tc--){
-            System.out.println(cometYear - (cometFly * tc));
-        }
-        for (int tc=1;tc <= timesCometFlewAfter;tc++){
-            System.out.println(cometYear + (cometFly * tc));
-        }
-
+        System.out.println(Arrays.toString(nums).replace("[", "").replace("]", ""));
     }
 }
