9971da718b0b1d79edea686b745bb9e6de7d4a27
Leetisdeep <aaaooogjg@gmail.com>
2025-05-19 09:41:21 +0300

    Методы объектов

--- src/Book.java
+++ src/Book.java
@@ -3,34 +3,74 @@ public class Book {
     private Author author;
     private int publicationYear;
 
-    public Book(String title,Author author,int publicationYear){
+    public Book(String title, Author author, int publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
     }
 
-    public String getTitle(){
-        return this.title;
+    public String getTitle() {
+        return title;
     }
 
-    public Author getAuthor(){
-        return this.author;
+    public Author getAuthor() {
+        return author;
     }
 
-    public int getPublicationYear(){
-        return this.publicationYear;
+    public int getPublicationYear() {
+        return publicationYear;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setAuthor(Author author){
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
-    public void setPublicationYear(int publicationYear){
+    public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
 
+    @Override
+    public String toString() {
+        // делегируем вызов toString автора
+        String authorStr = (author != null) ? author.toString() : "null";
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + authorStr +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true; 
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Book other = (Book) obj;
+
+        if (publicationYear != other.publicationYear)
+            return false;
+
+        if (title != null ? !title.equals(other.title) : other.title != null)
+            return false;
+
+        if (author != null ? !author.equals(other.author) : other.author != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17; // стартовое число
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + publicationYear;
+        return result;
+    }
 }
